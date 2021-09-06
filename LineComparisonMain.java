ca79a9be91a1bee92cc0055fc5f4a2530bd34fad
sushant2931 <sushantlad8888@gmail.com>
2021-09-06 19:50:49 +0530

    Updated Compare method with Object as Argument

--- LineComparisonMain.java
+++ LineComparisonMain.java
@@ -7,42 +7,52 @@ public class LineComparisonMain {
 
 		int x1, y1, x2, y2;
 		Scanner sc = new Scanner(System.in);
-		LineComparison line = new LineComparison();
+
 		// Read first co-ordinates of First line
 		System.out.println("Enter co-ordinates of First line");
+		
 		System.out.println("Enter first co-ordinates (x,y) ");
+		
 		System.out.print("Enter x : ");
-		x1 = sc.nextInt();
+			x1 = sc.nextInt();
+			
 		System.out.print("Enter y : ");
-		y1 = sc.nextInt();
+			y1 = sc.nextInt();
+			
 		// Read second co-ordinates
 		System.out.println("Enter second co-ordinates (x,y) ");
 		System.out.print("Enter x : ");
-		x2 = sc.nextInt();
+			x2 = sc.nextInt();
+			
 		System.out.print("Enter y : ");
-		y2 = sc.nextInt();
-
-		Double length1 = line.calcLength(x1, y1, x2, y2);
-		System.out.printf("Length of first line :%.2f ", length1);
+			y2 = sc.nextInt();
+			
+		LineComparison line1 = new LineComparison(x1, y1, x2, y2);
+		
+		System.out.printf("Length of first line :%.2f ", line1.getLength());
 
 		// Read first co-ordinates of Second line
 		System.out.println("\nEnter co-ordinates of Second line");
 		System.out.println("Enter first co-ordinates (x,y) ");
 		System.out.print("Enter x : ");
-		x1 = sc.nextInt();
+			x1 = sc.nextInt();
+			
 		System.out.print("Enter y : ");
-		y1 = sc.nextInt();
+			y1 = sc.nextInt();
+			
 		// Read second co-ordinates
 		System.out.println("Enter second co-ordinates (x,y) ");
 		System.out.print("Enter x : ");
-		x2 = sc.nextInt();
+			x2 = sc.nextInt();
+			
 		System.out.print("Enter y : ");
-		y2 = sc.nextInt();
+			y2 = sc.nextInt();
 
-		Double length2 = line.calcLength(x1, y1, x2, y2);
-		System.out.printf("Length of second line : %.2f ", length2);
+		LineComparison line2 = new LineComparison(x1, y1, x2, y2);
+		System.out.printf("Length of second line : %.2f ", line2.getLength());
 
-		line.compare(length1, length2);
-		sc.close();
+			line1.compare(line2);
+
+			sc.close();
 	}
 }
