4c860438afd79eb2199afb47577ca4e22986eef3
sushant2931 <sushantlad8888@gmail.com>
2021-09-06 19:22:38 +0530

    Welcome in Line Comparison Problem On Master Branch

---
+++ LineComparison.java
@@ -0,0 +1,10 @@
+package com.bl.linecomparison;
+
+import java.lang.Math;
+
+public class LineComparison {
+	// public double calcLength(int x1, int y1, int x2, int y2) {
+
+	// double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	// return length;
+}
